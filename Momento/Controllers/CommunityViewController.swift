85b799c4557fdfa07101621a237c053c944dbe9e
Sam Henry <henry51@comcast.net>
2019-02-13 21:28:50 -0700

    layout setup for tabbar controller

---
+++ Momento/Controllers/CommunityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CommunityViewController.swift
+//  Momento
+//
+//  Created by Sam Henry on 2/13/19.
+//  Copyright © 2019 Sam Henry. All rights reserved.
+//
+
+import UIKit
+
+class CommunityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
