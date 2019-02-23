5ed505c93aae6d0fd44e67c6a63dda67a66e9239
Sam Henry <henry51@comcast.net>
2019-02-23 12:57:23 -0700

    fixed app crashing - need to update pods and add firebase plist

--- Momento/Controllers/AppDelegate.swift
+++ Momento/Controllers/AppDelegate.swift
@@ -19,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         
+//        print("firebase: ", FirebaseApp.configure())
+        
         return true
     }
 
---
+++ Momento/Controllers/LoginProfileViewController.swift
@@ -0,0 +1,55 @@
+//
+//  LoginProfileViewController.swift
+//  Momento
+//
+//  Created by Sam Henry on 2/23/19.
+//  Copyright © 2019 Sam Henry. All rights reserved.
+//
+
+import UIKit
+
+class LoginProfileViewController: UIViewController {
+    
+    let bgImage = UIImageView()
+
+    @IBOutlet weak var emailInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    
+    
+    @IBAction func signupBtn(_ sender: UIButton) {
+        print("signup pressed")
+    }
+    
+    @IBAction func loginBtn(_ sender: UIButton) {
+        print("login pressed")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setBG()
+        // Do any additional setup after loading the view.
+    }
+    
+    func setBG (){
+        view.addSubview(bgImage)
+        bgImage.translatesAutoresizingMaskIntoConstraints = false
+        bgImage.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        bgImage.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        bgImage.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        bgImage.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        bgImage.image = UIImage(named: "overlay")
+        
+        view.sendSubviewToBack(bgImage)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
