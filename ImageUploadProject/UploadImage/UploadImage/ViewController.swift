b5c0ccc7445eb760eb3e434d9cafdf1b2183be93
Joshua Griffiths <joshuagriffiths@Joshuas-MacBook-Pro.local>
2019-03-08 13:33:44 -0700

    added coments

--- ImageUploadProject/UploadImage/UploadImage/ViewController.swift
+++ ImageUploadProject/UploadImage/UploadImage/ViewController.swift
@@ -16,43 +16,43 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBOutlet weak var uploadImage: UIImageView!
+    @IBOutlet weak var uploadImage: UIImageView!//From Main.Storyboard
     @IBOutlet weak var downloadImage: UIImageView!
     
-    let filename = "earth.jpg"
+    let filename = "earth.jpg"//name of the file that will appear in the database
     
     var imageReference: StorageReference {
-        return Storage.storage().reference().child("images")
+        return Storage.storage().reference().child("images")//create a bucket in database to store image
     }
     
     @IBAction func onUploadTapped() {
-        guard let image = uploadImage.image else { return }
-        guard let imageData = UIImageJPEGRepresentation(image, 1) else { return }
+        guard let image = uploadImage.image else { return }//refrence image from Main.storyboard
+        guard let imageData = UIImageJPEGRepresentation(image, 1) else { return }//whats going to be actually uploaded
         
-        let uploadImageRef = imageReference.child(filename)
+        let uploadImageRef = imageReference.child(filename)//where were gonna store the image data (in somethign called earth.jpg
         
         let uploadTask = uploadImageRef.putData(imageData, metadata: nil) { (metadata, error) in
             print("UPLOAD TASK FINISHED")
             print(metadata ?? "NO METADATA")
             print(error ?? "NO ERROR")
-        }
+        }//upload the image
         
         uploadTask.observe(.progress) { (snapshot) in
             print(snapshot.progress ?? "NO MORE PROGRESS")
-        }
+        }//logging
         
-        uploadTask.resume()
+        uploadTask.resume()//resume after printing
         
     }
     
     @IBAction func onDownloadTapped() {
-        let downloadImageRef = imageReference.child(filename)
+        let downloadImageRef = imageReference.child(filename)//where were gonna get the data from
         
         let downloadtask = downloadImageRef.getData(maxSize: 1024 * 1024 * 12) { (data, error) in
             if let data = data {
                 let image = UIImage(data: data)
                 self.downloadImage.image = image
-            }
+            }//get the data from firebase
             print(error ?? "NO ERROR")
         }
         
