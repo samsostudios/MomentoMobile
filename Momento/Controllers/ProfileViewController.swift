1f03707562b16f8183ccfc4f149bae657c3ad574
Sam Henry <henry51@comcast.net>
2019-03-20 15:45:23 -0600

    succefully downloading all user images to page

--- Momento/Controllers/ProfileViewController.swift
+++ Momento/Controllers/ProfileViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 import Firebase
 import FirebaseStorage
+import Photos
 
 class ProfileViewController: UIViewController, UITextViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
+    
+    var userImages = [UIImage]()
     
     @IBAction func importBtn(_ sender: UIButton) {
         let image = UIImagePickerController()
@@ -27,20 +30,48 @@ class ProfileViewController: UIViewController, UITextViewDelegate, UINavigationC
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        var imageName = ""
+        
+        if let asset = info[UIImagePickerController.InfoKey.phAsset] as? PHAsset {
+            let assetResources = PHAssetResource.assetResources(for: asset)
+            
+            print("FILENAME", assetResources.first!.originalFilename)
+            
+            imageName = assetResources.first!.originalFilename
+        }
+        
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            
             guard let imageData = image.jpegData(compressionQuality: 0.75) else { return }
             
             let imageMetaData = StorageMetadata()
             imageMetaData.contentType = "image/jpg"
-            let uid = Auth.auth().currentUser!.uid
             
-            let userContentRef = Storage.storage().reference().child(uid)
-            let uploadTask = userContentRef.child("images").putData(imageData, metadata: imageMetaData) { (metaData, error) in
+            let imgNameSeperated = imageName.components(separatedBy: ".")
+            print("SEPERATED!", imgNameSeperated)
+            
+            let uid = Auth.auth().currentUser!.uid
+            let userContentRef = Storage.storage().reference().child(uid).child("images")
+            let contentDBRef = Database.database().reference().child("Users").child(uid).child("Content").child(imgNameSeperated[0])
+            
+            
+            let uploadTask = userContentRef.child(imageName).putData(imageData, metadata: imageMetaData) { (metaData, error) in
                 
                 if error != nil{
                     print("ADD ALERT for failed upload")
                 }else{
                     print("Upload Complete!")
+                    userContentRef.child(imageName).downloadURL {
+                        (imgURL, error) in
+                        
+                        if error != nil{
+                            print("ADD ALERT for errors")
+                        }else{
+                            print("URL:", imgURL!)
+                            let downloadUrl = imgURL?.absoluteString
+                            contentDBRef.setValue(downloadUrl)
+                        }
+                    }
                 }
             }
             uploadTask.observe(.progress) { (snapshot) in
@@ -52,7 +83,6 @@ class ProfileViewController: UIViewController, UITextViewDelegate, UINavigationC
         }
         
         self.dismiss(animated: true, completion: nil)
-        
         
     }
     
@@ -66,13 +96,54 @@ class ProfileViewController: UIViewController, UITextViewDelegate, UINavigationC
         }
     }
     
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.setNavigationBarHidden(true, animated: true)
         
-        let user = Auth.auth().currentUser!.uid
-        print("profile for: ", user)
-        // Do any additional setup after loading the view.
+        getUserImages()
+    }
+    
+    func getUserImages() {
+        print("getting images")
+        let uid = Auth.auth().currentUser!.uid
+        let contentDBRef = Database.database().reference().child("Users").child(uid).child("Content")
+        
+        contentDBRef.observeSingleEvent(of: .value, with: {(snapshot) in
+            
+            for item in snapshot.children.allObjects as! [DataSnapshot]{
+                
+                let downloadLink = item.value as! String
+                let storageRef = Storage.storage().reference(forURL: downloadLink)
+                
+                storageRef.downloadURL(completion: { (url, error) in
+                    do{
+                        let data = try Data(contentsOf: url!)
+                        
+                        let newImage = UIImage(data: data as Data)
+                        
+                        DispatchQueue.main.async {
+                            self.displayImages(image: newImage!)
+                        }
+                        
+                    }catch{
+                        print("error with data")
+                    }
+                })
+            }
+        })
+    }
+    
+    func displayImages(image: UIImage) {
+        print("display images", image)
+        self.userImages.append(image)
+        print("user images", self.userImages)
+        
+        self.imageView.image = self.userImages[0]
     }
     
 
